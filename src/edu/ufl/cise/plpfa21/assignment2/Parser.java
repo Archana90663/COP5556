36dda76b8b3296fa2cf0d8bffff62febf5f0ef6f
Archana90663 <adhyani@wisc.edu>
2021-10-04 11:39:47 -0400

    Added some more methods

--- src/edu/ufl/cise/plpfa21/assignment2/Parser.java
+++ src/edu/ufl/cise/plpfa21/assignment2/Parser.java
@@ -1,7 +1,5 @@
 package edu.ufl.cise.plpfa21.assignment2;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.*;
 
 import edu.ufl.cise.plpfa21.assignment1.IPLPToken;
@@ -10,14 +8,19 @@ import edu.ufl.cise.plpfa21.assignment1.LexicalException;
 import edu.ufl.cise.plpfa21.assignment1.PLPTokenKinds;
 import edu.ufl.cise.plpfa21.assignment1.Token;
 import edu.ufl.cise.plpfa21.assignment3.ast.IASTNode;
+import edu.ufl.cise.plpfa21.assignment3.ast.IDeclaration;
 import edu.ufl.cise.plpfa21.assignment3.ast.IExpression;
-import edu.ufl.cise.plpfa21.assignment3.astimpl.BinaryExpression__;
+import edu.ufl.cise.plpfa21.assignment3.ast.IStatement;
+import edu.ufl.cise.plpfa21.assignment3.astimpl.*;
+import edu.ufl.cise.plpfa21.assignment3.astimpl.Block__;
 import edu.ufl.cise.plpfa21.assignment3.astimpl.BooleanLiteralExpression__;
+import edu.ufl.cise.plpfa21.assignment3.astimpl.Declaration__;
 import edu.ufl.cise.plpfa21.assignment3.astimpl.Expression__;
 import edu.ufl.cise.plpfa21.assignment3.astimpl.IdentExpression__;
 import edu.ufl.cise.plpfa21.assignment3.astimpl.Identifier__;
 import edu.ufl.cise.plpfa21.assignment3.astimpl.IntLiteralExpression__;
 import edu.ufl.cise.plpfa21.assignment3.astimpl.Program__;
+import edu.ufl.cise.plpfa21.assignment3.astimpl.Statement__;
 import edu.ufl.cise.plpfa21.assignment1.PLPTokenKinds.Kind;
 
 
@@ -25,6 +28,9 @@ public class Parser implements IPLPParser{
 
 	public IPLPToken token;
 	public Lexer lexer;
+	
+	HashSet<Kind> declarations = new HashSet<>();
+	HashSet<Kind> statements = new HashSet<>();
 	
 	
 	public Parser(Lexer lexer) {
@@ -35,8 +41,11 @@ public class Parser implements IPLPParser{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	
+		declarations.add(Kind.KW_INT);
+		declarations.add(Kind.KW_BOOLEAN);
+		statements.add(Kind.KW_WHILE);
+		statements.add(Kind.KW_IF);
+		statements.add(Kind.IDENTIFIER);
 		
 	}
 
@@ -149,6 +158,210 @@ public class Parser implements IPLPParser{
 		}
 		return ex;
 			
+	}
+	
+	public Block__ blck() throws SyntaxException{
+		IPLPToken first = this.token;
+		Block__ b;
+		List<IDeclaration> declarationList = new ArrayList<>();
+		List<IStatement> statementList = new ArrayList<>();
+		
+		try {
+			nextOne();
+			while(declarations.contains(this.token.getKind()) || statements.contains(this.token.getKind())) {
+				if(declarations.contains(this.token.getKind())) {
+					IDeclaration declaration = declarationMethod();
+					declarationList.add(declaration);
+					
+				}
+				else if(statements.contains(this.token.getKind())) {
+					IStatement s = stmt();
+					statementList.add(s);
+				}
+				else {
+					throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+				}
+			}
+			b = new Block__(first.getLine(), first.getCharPositionInLine(), first.getText(), statementList);
+		} catch(SyntaxException e) {
+			throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+
+		}
+		return b;
+		
+	}
+	
+	public IDeclaration declarationMethod() throws SyntaxException{
+		IPLPToken first = this.token;
+		IDeclaration d;
+		if(this.token.getKind()==Kind.KW_INT || this.token.getKind() == Kind.KW_BOOLEAN) {
+			try {
+				nextOne();
+				IPLPToken i = equal(Kind.IDENTIFIER);
+				d = new Declaration__(first.getLine(), first.getCharPositionInLine(), first.getText());
+			} catch(SyntaxException e) {
+				throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+			}
+		}
+		else {
+			throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+		}
+		return d;
+	}
+	
+	public Program__ prgm() throws SyntaxException{
+		IPLPToken first = this.token;
+		Program__ p;
+		List<IDeclaration> list = new ArrayList<>();
+		try {
+			equal(Kind.IDENTIFIER);
+			Declaration__ dec;
+			if(this.token.getKind() == Kind.KW_INT || this.token.getKind() == Kind.KW_BOOLEAN) {
+				dec = prmDec();
+				list.add(dec);
+				while(this.token.getKind() == Kind.COMMA) {
+					nextOne();
+					dec = prmDec();
+					list.add(dec);
+				}	
+			}
+			//int line, int posInLine, String text, List<IDeclaration> declarations
+			p = new Program__(first.getLine(), first.getCharPositionInLine(), first.getText(), list);
+			
+		} catch(SyntaxException e) {
+			throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+
+		}
+		return p;
+	}
+	
+	public Declaration__ prmDec() throws SyntaxException{
+		IPLPToken first = this.token;
+		Declaration__ dec;
+		if(this.token.getKind() == Kind.KW_INT || this.token.getKind() == Kind.KW_BOOLEAN) {
+			try {
+				nextOne();
+				IPLPToken i = equal(Kind.IDENTIFIER);
+				dec = new Declaration__(0,0,null);
+			} catch(SyntaxException e) {
+				throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+
+			}
+		}
+		else {
+			throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+		}
+		return dec;
+	}
+	
+	public IStatement stmt() throws SyntaxException{
+		IPLPToken first = this.token;
+		IStatement s;
+		Kind kind = this.token.getKind();
+		switch(kind) {
+			case KW_WHILE:
+				try {
+					s = whileStatementMethod();
+				} catch(SyntaxException e) {
+					throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+				}
+				break;
+			case KW_IF:
+				try {
+					s = ifStatementMethod();
+				} catch(SyntaxException e) {
+					throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+				}
+				break;
+			case IDENTIFIER:
+				try {
+					IPLPToken t = lexer.nextToken();
+					if(t.getKind() == Kind.ASSIGN) {
+						s = assignStatementMethod();
+						equal(Kind.SEMI);
+						
+					}
+					else {
+						throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+					}
+					
+				} catch(SyntaxException | LexicalException e) {
+					throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+
+				}
+				break;
+			default:
+				throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+		}
+		return s;
+	}
+	
+	public AssignmentStatement__ assignStatementMethod() throws SyntaxException{
+		IPLPToken first = this.token;
+		AssignmentStatement__ as;
+		if(this.token.getKind() == Kind.IDENTIFIER) {
+			try {
+				IPLPToken i = this.token;
+				nextOne();
+				equal(Kind.ASSIGN);
+				IExpression ex = expr();
+				Identifier__ ident = new Identifier__(first.getLine(), first.getCharPositionInLine(), first.getStringValue(), first.getText());
+				IdentExpression__ identValue = new IdentExpression__(i.getLine(), i.getCharPositionInLine(), i.getText(), ident);
+				as = new AssignmentStatement__(first.getLine(), first.getCharPositionInLine(), first.getText(), identValue, ex);
+				
+			} catch(SyntaxException e) {
+				throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+
+			}
+		}
+		else {
+			throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+		}
+		return as;
+	}
+	
+	public WhileStatement__ whileStatementMethod() throws SyntaxException{
+		IPLPToken first = this.token;
+		WhileStatement__ ws;
+		if(this.token.getKind() == Kind.KW_WHILE) {
+			try {
+				nextOne();
+				equal(Kind.LPAREN);
+				IExpression ex = expr();
+				equal(Kind.RPAREN);
+				Block__ b = blck();
+				ws = new WhileStatement__(first.getLine(), first.getCharPositionInLine(), first.getText(), ex, b);
+			} catch(SyntaxException e) {
+				throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+			}
+		}
+		else {
+			throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+
+		}
+		return ws;
+	}
+	
+	public IfStatement__ ifStatementMethod() throws SyntaxException{
+		IPLPToken first = this.token;
+		IfStatement__ is;
+		if(this.token.getKind() == Kind.KW_IF) {
+			try {
+				nextOne();
+				equal(Kind.LPAREN);
+				IExpression ex = expr();
+				equal(Kind.RPAREN);
+				Block__ b = blck();
+				is = new IfStatement__(first.getLine(), first.getCharPositionInLine(), first.getText(), ex, b);
+				
+			} catch(SyntaxException e) {
+				throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+			}
+		}
+		else {
+			throw new SyntaxException("",this.token.getLine(), this.token.getCharPositionInLine());
+		}
+		return is;
 	}
 	
 	
